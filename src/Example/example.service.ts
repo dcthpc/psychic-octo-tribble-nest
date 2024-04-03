500aa8dfbd5f81135e56d3c86597b6016b2ff281
kalysta <kalysta.wy.wong@dct-cloud.com>
2024-04-03 11:10:31 +0800

    feat: :sparkles: check if char exist

--- src/Example/example.service.ts
+++ src/Example/example.service.ts
@@ -82,4 +82,40 @@ export class ExampleService {
 
     return Math.max(ans,totalCount);
   };
+
+  exist(board: string[][], word: string): boolean {
+    let ans = false
+
+    const m = board.length
+    const n = board[0].length
+    const total = word.length
+
+    const track = (i, j, l) => {
+      if (l === total) {
+        ans = true
+        return
+      }
+
+      if (i >= m || j >= n || i < 0 || j < 0) return
+      if (board[i][j] !== word[l]) return
+
+      const pre = board[i][j]
+      board[i][j] = '*'
+
+      track(i + 1, j, l + 1)
+      track(i, j + 1, l + 1)
+      track(i - 1, j, l + 1)
+      track(i, j - 1, l + 1)
+
+      board[i][j] = pre
+    }
+
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        track(i, j, 0)
+      }
+    }
+
+    return ans
+  }
 }
