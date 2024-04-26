6b5d91580d7be337d2510926210ab0d60a9b4bb8
kalysta <kalysta.wy.wong@dct-cloud.com>
2024-04-26 17:34:52 +0800

    feat: :sparkles: add min falling path

--- src/Example/example.service.ts
+++ src/Example/example.service.ts
@@ -129,4 +129,22 @@ export class ExampleService {
     }
     return seconds;
   };
+
+  minFallingPathSum(grid: number[][]): number {
+    const n = grid.length;
+    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(Infinity));
+    dp[0] = grid[0];
+
+    for (let i = 1; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            for (let k = 0; k < n; ++k) {
+                if (k === j) continue;
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + grid[i][j]);
+            }
+        }
+    }
+
+    return Math.min(...dp[n - 1]);
+};
+
 }
