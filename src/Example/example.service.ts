b667be25d7741710f78abdda5f67a48f095fe313
kalysta <kalysta.wy.wong@dct-cloud.com>
2024-06-03 10:57:17 +0800

    feat: :sparkles: add append character

--- src/Example/example.service.ts
+++ src/Example/example.service.ts
@@ -177,6 +177,22 @@ export class ExampleService {
       }, {})
 
     return score.map((position, index) => ranking[position])
-};
+  };
+
+  appendCharacters(s: string, t: string): number {
+    const n = t.length;
+    let tPointer = 0;
+
+    // Iterate through the characters of s using an index-based loop
+    for (let sPointer = 0; sPointer < s.length; sPointer++) {
+        // Check if the characters match
+        if (tPointer < n && s[sPointer] === t[tPointer]) {
+            tPointer++;
+        }
+    }
+
+    // Return the number of characters that need to be appended to s
+    return n - tPointer;
+  };
 
 }
