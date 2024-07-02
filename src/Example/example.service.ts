ef43e0e9fc069038e3e25ca5c1347aebf47d2742
kalysta <kalysta.wy.wong@dct-cloud.com>
2024-07-02 16:32:11 +0800

    add array intersect function

--- src/Example/example.service.ts
+++ src/Example/example.service.ts
@@ -195,4 +195,27 @@ export class ExampleService {
     return n - tPointer;
   };
 
+  function intersect(nums1, nums2) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let x = 0;
+    let y = 0;
+    const result = [];
+
+    while (x < nums1.length && y < nums2.length) {
+        if (nums1[x] < nums2[y]) {
+            x++;
+        } else if (nums1[x] > nums2[y]) {
+            y++;
+        } else {
+            result.push(nums1[x]);
+            x++;
+            y++;
+        }
+    }
+
+    return result;
+};
+
 }
